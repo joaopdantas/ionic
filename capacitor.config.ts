5486521a6b33e3d46b4b80a884718bf4b59462d5
joaopdantas <90097888+joaopdantas@users.noreply.github.com>
2022-07-03 16:23:47 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pt.agomes.ei3',
+  appName: 'Preço Baixo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
